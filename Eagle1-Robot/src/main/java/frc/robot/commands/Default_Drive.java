e9268dc013f2d5d7a38e0351dd1ed8103eb8a32e
Jack Garzella <7094537+jmgarzella@users.noreply.github.com>
2020-02-04 14:40:18 -0700

    Updated Command.java to have "default_drive" for arcade drive

--- Eagle1-Robot/src/main/java/frc/robot/commands/Default_Drive.java
+++ Eagle1-Robot/src/main/java/frc/robot/commands/Default_Drive.java
@@ -8,25 +8,36 @@ e modified and shared by FRC teams. The code   */
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.Drive_SubSystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+// import java.util.function.DoubleSupplier;
+
+import frc.robot.subsystems.Drive_SubSystem;
 
 /**
  * An example command that uses an example subsystem.
  */
-public class ExampleCommand extends CommandBase {
+public class Default_Drive extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
+  private final Drive_SubSystem m_drive;
+  private final Double m_forward;
+  private final Double m_rotation;
+  // private final DoubleSupplier m_forward;
+  // private final DoubleSupplier m_rotation;
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  // if Using double supplier 
+  //   public Default_Drive (Drive_SubSystem subsystem, DoubleSupplier forward, doubleSupplier rotation) {
+
+  public Default_Drive (Drive_SubSystem subsystem, Double forward, double rotation) {
+    m_drive = subsystem;
+    m_forward = forward;
+    m_rotation = rotation;
+
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(m_drive);
   }
 
   // Called when the command is initially scheduled.
@@ -37,6 +48,10 @@ public class ExampleCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    m_drive.arcadeDrive(m_forward, m_rotation);
+    // if using doublesupplier   
+    //  m_drive.arcadeDrive(m_forward.getAsDouble(), m_rotation.getAsDouble());
+    // 
   }
 
   // Called once the command ends or is interrupted.
