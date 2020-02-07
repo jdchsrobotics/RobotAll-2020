6d0f4b488520990517a2a33f42e4935f84a2edb6
jdchsrobotics <47485490+jdchsrobotics@users.noreply.github.com>
2020-02-07 14:11:44 -0700

    Mapped out color wheel and climber buttons

---
+++ Eagle1-Robot/src/main/java/frc/robot/subsystems/ColorWheelSubSystem.java
@@ -0,0 +1,11 @@
+/*
+1. Extend arm - Start button
+2. Retract arm - Menu Button
+3. Spin Wheel
+    a. blue - X - button
+    b. red - B - button
+    c. yellow - Y - button
+    d. green - A - button
+
+Total of 6 buttons
+*/
---
+++ Eagle1-Robot/src/main/java/frc/robot/subsystems/LifterSubSystem.java
@@ -0,0 +1,12 @@
+/*
+Climber Comments:
+
+Commands:
+1. Raise Bar (RB)
+2. Lower Bar (LB)
+3. Balance Left (LT)
+4. Balance Right (RT)
+5. Bar Lock/Unlock (L3)
+
+Total of 5 buttons
+*/
