9e23730ae3aa05b037d0548a91f4909d014fca07
Jack Garzella <7094537+jmgarzella@users.noreply.github.com>
2020-02-17 22:13:48 -0700

    Added Ball mangement documentation

--- Eagle1-Robot/src/main/java/frc/robot/subsystems/BallManagementSubSystem.java
+++ Eagle1-Robot/src/main/java/frc/robot/subsystems/BallManagementSubSystem.java
@@ -1,7 +1,14 @@
 
-
-
-
+/*****
+ *   USed for Ball SubSystem Management
+ * 
+ * MOTIONS: 
+ *  1.  Flip Forward  (probalby bosh windows motors)
+ *  2.  Flip UP/Back  
+ *  3.  Pull in Balls  (Forward on Motors (Probably Talons SPX))
+ *  4.  Push OUt Balls (Reverse on Motors (probably Taoons SPX))
+ * 
+ */
 
 package frc.robot.subsystems;
 
